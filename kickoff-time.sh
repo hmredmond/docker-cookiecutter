4c120e613393880753dd1d0095112a82b7c8a9dd
Hannah Redmond <hannah.redmond@statsbomb.com>
2024-04-14 22:45:07 +0100

    access remote file

--- kickoff-time.sh
+++ kickoff-time.sh
@@ -5,7 +5,7 @@
 PAT="$1"
 echo "PAT: $1"
 # Build the Docker image with the provided PAT
-docker build --build-arg PAT="$1" -t cookiecutter-image-4 .
+docker build --build-arg PAT="$1" -t kitbag-kickoff-cookiecutter-image -f https://raw.githubusercontent.com/hmredmond/docker-cookiecutter/main/Dockerfile .
 
 # Run the Docker container
-docker run -it -v "$(pwd)":/app cookiecutter-image-4 https://github.com/statsbomb/kitbag-kickoff.git
+docker run -it -v "$(pwd)":/app kitbag-kickoff-cookiecutter-image https://github.com/statsbomb/kitbag-kickoff.git
