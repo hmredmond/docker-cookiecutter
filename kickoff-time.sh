9366cbb2bd809340fb39b474d5b50282da321018
Hannah Redmond <hannah.redmond@statsbomb.com>
2024-04-15 09:18:13 +0100

    reverted to curl

--- kickoff-time.sh
+++ kickoff-time.sh
@@ -6,3 +6,6 @@ PAT="$1"
 echo "PAT: $1"
 # Build the Docker image with the provided PAT
 docker build --build-arg PAT="$1" -t kitbag-kickoff-cookiecutter-image -f https://raw.githubusercontent.com/hmredmond/docker-cookiecutter/main/Dockerfile .
+
+# Run the Docker container
+docker run -it -v "$(pwd)":/app kitbag-kickoff-cookiecutter-image https://github.com/statsbomb/kitbag-kickoff.git
