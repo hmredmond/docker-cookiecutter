3864616c6d16e58baacb59ee8bbc5bf2f79a6555
Hannah Redmond <hannah.redmond@statsbomb.com>
2024-04-15 16:19:09 +0100

    trying to get things working remotely

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const { exec } = require('child_process');
-
-// inspired by https://github.com/piuccio/cowsay
+const https = require('https');
+const fs = require('fs');
 
 // Access the command-line arguments
 const args = process.argv.slice(2);
@@ -15,15 +15,38 @@ if (args.length === 0) {
 // Get the value of the argument
 const myArgument = args[0];
 
-// Execute the shell script
-exec(`sh ./kickoff-time.sh ${myArgument}`, (error, stdout, stderr) => {
-  if (error) {
-    console.error(`Error executing the script: ${error.message}`);
+// Download the shell script
+const scriptUrl = 'https://raw.githubusercontent.com/hmredmond/docker-cookiecutter/main/kickoff-time.sh';
+const scriptPath = './kickoff-time.sh';
+
+https.get(scriptUrl, (response) => {
+  if (response.statusCode !== 200) {
+    console.error(`Failed to download the script. Status code: ${response.statusCode}`);
     return;
   }
-  if (stderr) {
-    console.error(`Script execution returned an error: ${stderr}`);
-    return;
-  }
-  console.log(`Build Script executed successfully. Output:\n${stdout}`);  
+
+  const fileStream = fs.createWriteStream(scriptPath);
+  response.pipe(fileStream);
+
+  fileStream.on('finish', () => {
+    fileStream.close();
+
+    // Make the script executable
+    fs.chmodSync(scriptPath, '755');
+
+    // Execute the shell script
+    exec(`sh ${scriptPath} ${myArgument}`, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error executing the script: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.error(`Script execution returned an error: ${stderr}`);
+        return;
+      }
+      console.log(`Script executed successfully. Output:\n${stdout}`);
+    });
+  });
+}).on('error', (error) => {
+  console.error(`Error downloading the script: ${error.message}`);
 });
